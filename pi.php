2f170246ffc8a9c9d2bb0e3216816febf2526e35
merwine32 <35571684+merwine32@users.noreply.github.com>
2018-03-06 16:46:33 -0500

    Update pi.php

--- pi.php
+++ pi.php
@@ -1,6 +1,4 @@
-<php
-     //3 + 4/(2*3*4) - 4/(4*5*6) + 4/(6*7*8) - 4/(8*9*10) + 4/(10*11*12) - 4/(12*13*14)
-
+<?php
 $num = 0;
 $consecutive1 = 2;
 $consecutive2 = 3;
@@ -10,7 +8,7 @@ $first = 0;
 $final = 0;
 $t = 0;
 
-while($t < 100000){
+while($t < 10){
 
 if($first == 0){
 
@@ -36,25 +34,25 @@ $consecutive1 = $consecutive3;
 $consecutive2 = $consecutive1 + 1;
 $consecutive3 = $consecutive2 + 1;
 
-$t++
+$t++;
 }
 
 }elseif(opperation == 1){
 
 if($first == 0){
 
-$final = $final 4/($consecutive1 * consecutive2 * consecutive3);
+$final = $final - 4/($consecutive1 * consecutive2 * consecutive3);
 
 $consecutive1 = $consecutive3;
 $consecutive2 = $consecutive1 + 1;
 $consecutive3 = $consecutive2 + 1;
 
-$t++
+$t++;
 }
 
 }else{
 
-echo "Error!"
+echo "Error!";
 
 }
 
